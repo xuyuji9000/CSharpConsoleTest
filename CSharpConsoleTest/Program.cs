002386d7786923b27c9915cddc4642ceae3f2403
xuyuji9000 <xuyuji9000@163.com>
2014-03-19 12:03:13 +0800

    test

--- CSharpConsoleTest/Program.cs
+++ CSharpConsoleTest/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace CSharpConsoleTest
 {
+
     class Program
     {
+        class tt
+        {
+            public void Print1()
+            {
+                Console.WriteLine("the delegate type which is equal");
+            }
+            public void Print2()
+            {
+                Console.WriteLine("the change of the future");
+            }
+        }
+        delegate void PrintFunction();
         static void Main(string[] args)
         {
+            tt t = new tt();
+            PrintFunction pf = t.Print1;
+            pf += t.Print2;
+            pf();
+            
         }
     }
 }
